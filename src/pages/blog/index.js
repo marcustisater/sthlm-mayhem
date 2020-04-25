09802b75626b32822a6091236db0c56970cf9074
Marcus Tisäter <marcus.tisater@nordicmorning.com>
2020-04-25 14:50:19 +0200

    Rm unused import

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import TavlingarRoll from '../../components/TavlingarRoll'
 
-import '../../components/Sponsors';
 import Sponsors from '../../components/Sponsors';
 
 import '../../components/test.css';
